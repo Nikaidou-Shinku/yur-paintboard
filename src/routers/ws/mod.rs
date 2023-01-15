508c0288bd5f55a8127e5031daca8317a0a91505
yurzhang <yurzhang@163.com>
2023-01-16 02:54:44 +0800

    refact: ws

--- src/routers/ws/mod.rs
+++ src/routers/ws/mod.rs
@@ -1,16 +1,18 @@
 mod read;
-mod write;
 
-use std::sync::Arc;
+use std::{sync::Arc, time::Duration};
 
-use uuid::Uuid;
-use futures::{StreamExt, SinkExt};
-use axum::{extract::{State, WebSocketUpgrade, ws::{WebSocket, Message}}, response::Response};
+use axum::{
+  extract::{
+    State,
+    WebSocketUpgrade,
+    ws::{WebSocket, Message},
+  },
+  response::Response,
+};
 
-use tracing::Instrument;
-
-use crate::{AppState, channel::ChannelMsg};
-use self::{read::ws_read, write::ws_write};
+use crate::AppState;
+use read::ws_read;
 
 pub async fn ws(
   State(state): State<Arc<AppState>>,
@@ -19,86 +21,84 @@ pub async fn ws(
   ws.on_upgrade(|socket| handle_ws(state, socket))
 }
 
+pub struct WsState {
+  uid: Option<i32>,
+  readonly: bool,
+  get_pong: bool,
+  quick_paint: u8,
+  trash_pack: u8,
+}
+
 #[tracing::instrument(name = "ws", skip_all, fields(uid))]
 async fn handle_ws(
   state: Arc<AppState>,
-  socket: WebSocket,
+  mut socket: WebSocket,
 ) {
-  let (mut ws_out, mut ws_in) = socket.split();
+  let mut receiver = state.sender.subscribe();
+  let mut ws_paints = vec![];
+  // TODO(config)
+  let mut interval = tokio::time::interval(Duration::from_millis(250));
+  let mut heartbeat = tokio::time::interval(Duration::from_secs(20));
 
-  let uid: i32;
-
-  loop { // wait for auth
-    let msg = ws_in.next().await;
-
-    if msg.is_none() {
-      return;
-    }
-
-    let msg = msg.unwrap();
-
-    if msg.is_err() {
-      continue;
-    }
-
-    let msg = msg.unwrap();
-
-    let res = ws_read(state.clone(), None, None, msg).await;
-
-    if let Some(res) = res {
-      uid = res;
-      break;
-    }
-
-    let res = ws_out.send(Message::Binary(vec![0xfd])).await; // auth failed
-
-    if res.is_err() {
-      return;
-    }
-  }
-
-  tracing::Span::current()
-    .record("uid", uid);
-
-  let res = ws_out.send(Message::Binary(vec![0xfc])).await; // auth success
-
-  if res.is_err() {
-    return;
-  }
-
-  tracing::info!("Authenticated.");
-
-  let ws_session = Uuid::new_v4();
-
-  let read_task = async {
-    loop {
-      let msg = ws_in.next().await;
-
-      if msg.is_none() {
-        break;
-      }
-
-      let msg = msg.unwrap();
-
-      if msg.is_err() {
-        continue;
-      }
-
-      let msg = msg.unwrap();
-
-      ws_read(state.clone(), Some(uid), Some(ws_session), msg).await;
-    }
-
-    state.sender.send(ChannelMsg::Close(ws_session)).unwrap();
-
-    tracing::trace!("Closed.");
+  let mut ws_state = WsState {
+    uid: None,
+    readonly: true,
+    get_pong: false,
+    quick_paint: 0,
+    trash_pack: 0,
   };
 
-  let read_task = read_task.instrument(tracing::info_span!("read"));
+  loop {
+    tokio::select! {
+      msg = socket.recv() => {
+        let end = ws_read(state.clone(), &mut socket, &mut ws_state, msg).await;
+        if end {
+          break;
+        }
+      },
+      msg = receiver.recv() => {
+        if let Ok(paint) = msg {
+          if !ws_state.readonly {
+            ws_paints.push(paint);
+          }
+        }
+      },
+      _ = interval.tick() => {
+        if ws_paints.len() > 0 {
+          let mut msg = Vec::with_capacity(ws_paints.len() * 7 + 1);
+  
+          msg.push(0xfa);
 
-  let write_task = ws_write(state.clone(), ws_session, ws_out);
+          for pixel in ws_paints.iter() {
+            let pixel_bytes: [u8; 7] = pixel.into();
+            msg.extend_from_slice(&pixel_bytes);
+          }
 
-  futures::future::join(read_task, write_task).await;
+          ws_paints.clear();
+
+          let res = socket.send(Message::Binary(msg)).await;
+  
+          if res.is_err() {
+            break;
+          }
+        }
+      },
+      _ = heartbeat.tick() => {
+        let res = socket.send(Message::Binary(vec![0xf8])).await;
+        if res.is_err() {
+          break;
+        }
+
+        tokio::time::sleep(Duration::from_secs(10)).await;
+
+        if !ws_state.get_pong {
+          break;
+        }
+
+        ws_state.get_pong = false;
+      },
+    }
+  }
 
   tracing::info!("Closed.");
 }
