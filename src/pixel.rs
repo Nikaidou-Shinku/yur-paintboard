d46727d99f428c58f64d7f2f6e40b4feef139da2
yurzhang <yurzhang@163.com>
2023-01-16 00:16:32 +0800

    feat: optimize `0xf9`

--- src/pixel.rs
+++ src/pixel.rs
@@ -1,5 +1,3 @@
-use bytes::{Bytes, BytesMut, BufMut};
-
 #[derive(Clone, Debug)]
 pub struct Pixel {
   pub x: u16,
@@ -7,17 +5,17 @@ pub struct Pixel {
   pub color: (u8, u8, u8),
 }
 
-impl From<&Pixel> for Bytes {
+impl From<&Pixel> for [u8; 7] {
   fn from(pixel: &Pixel) -> Self {
-    let mut res = BytesMut::with_capacity(7);
+    let mut res = [0; 7];
 
-    res.put_u16_le(pixel.x);
-    res.put_u16_le(pixel.y);
+    res[0..2].copy_from_slice(&pixel.x.to_le_bytes());
+    res[2..4].copy_from_slice(&pixel.y.to_le_bytes());
 
     let c = pixel.color;
-    res.put_slice(&[c.0, c.1, c.2]);
-
-    res.freeze()
+    res[4..7].copy_from_slice(&[c.0, c.1, c.2]);
+    
+    res
   }
 }
 
@@ -31,12 +29,4 @@ pub fn hex_to_bin(hex: &str) -> [u8; 3] {
   let b = u8::from_str_radix(&hex[5..7], 16).unwrap();
 
   [r, g, b]
-}
-
-pub fn hex_to_bytes(hex: &str) -> Bytes {
-  let r = u8::from_str_radix(&hex[1..3], 16).unwrap();
-  let g = u8::from_str_radix(&hex[3..5], 16).unwrap();
-  let b = u8::from_str_radix(&hex[5..7], 16).unwrap();
-
-  Bytes::from(vec![r, g, b])
 }
--- src/routers/ws/write.rs
+++ src/routers/ws/write.rs
@@ -1,12 +1,11 @@
 use std::sync::Arc;
 
-use bytes::{BytesMut, BufMut, Buf, Bytes};
 use uuid::Uuid;
 use futures::{stream::SplitSink, SinkExt};
 use tokio::sync::Mutex;
 use axum::extract::ws::{Message, WebSocket};
 
-use yur_paintboard::{consts::{WIDTH, HEIGHT}, pixel::hex_to_bytes};
+use yur_paintboard::{consts::{WIDTH, HEIGHT}, pixel::hex_to_bin};
 use crate::{AppState, channel::ChannelMsg};
 
 #[tracing::instrument(name = "write", skip_all)]
@@ -18,6 +17,7 @@ pub async fn ws_write(
   let ws_over = Mutex::new(false);
   let ws_paints = Mutex::new(vec![]);
   let ws_out = Mutex::new(ws_out);
+  let mut ws_readonly = true;
 
   let recv_task = async {
     let mut receiver = state.sender.subscribe();
@@ -43,11 +43,16 @@ pub async fn ws_write(
           }
         },
         ChannelMsg::Paint(paint) => {
-          ws_paints.lock().await.push(paint);
+          if !ws_readonly {
+            ws_paints.lock().await.push(paint);
+          }
         },
         ChannelMsg::Board(session) => {
           if session == ws_session {
             let board = get_board(state.clone());
+
+            ws_readonly = false;
+
             let res = ws_out.lock().await
               .send(Message::Binary(board)).await;
 
@@ -69,27 +74,34 @@ pub async fn ws_write(
         break;
       }
 
-      {
+      let msg = {
         let mut ws_paints = ws_paints.lock().await;
 
         if ws_paints.len() > 0 {
-          let mut msg = BytesMut::with_capacity(ws_paints.len() * 7 + 1);
+          let mut msg = Vec::with_capacity(ws_paints.len() * 7 + 1);
 
-          msg.put_u8(0xfa);
+          msg.push(0xfa);
 
           for pixel in ws_paints.iter() {
-            msg.put::<Bytes>(pixel.into());
-          }
-
-          let res = ws_out.lock().await
-            .send(Message::Binary(msg.into())).await;
-
-          if res.is_err() {
-            *ws_over.lock().await = true;
-            break;
+            let pixel_bytes: [u8; 7] = pixel.into();
+            msg.extend_from_slice(&pixel_bytes);
           }
 
           ws_paints.clear();
+
+          Some(msg)
+        } else {
+          None
+        }
+      };
+
+      if let Some(msg) = msg {
+        let res = ws_out.lock().await
+          .send(Message::Binary(msg)).await;
+
+        if res.is_err() {
+          *ws_over.lock().await = true;
+          break;
         }
       }
 
@@ -100,7 +112,7 @@ pub async fn ws_write(
 
   futures::future::join(recv_task, send_task).await;
 
-  tracing::info!("Closed.");
+  tracing::trace!("Closed.");
 }
 
 // TODO: maybe more elegant way to do this
@@ -108,21 +120,21 @@ fn get_board(
   state: Arc<AppState>,
 ) -> Vec<u8> {
   let max_len = WIDTH as usize * HEIGHT as usize * 3;
-  let mut board = BytesMut::with_capacity(max_len);
+  let mut board = Vec::with_capacity(max_len);
 
   for x in 0..WIDTH {
     for y in 0..HEIGHT {
       let pixel = state.board
         .get(&(x, y)).unwrap()
         .lock();
-      board.put(hex_to_bytes(&pixel.color));
+      let pixel_bytes = hex_to_bin(&pixel.color);
+      board.extend_from_slice(&pixel_bytes);
     }
   }
 
   // TODO(config): compress level
-  let raw_board = zstd::encode_all(board.reader(), 19).unwrap();
-  let mut board = vec![0xfb];
-  board.extend_from_slice(&raw_board);
+  let mut board = zstd::encode_all(board.as_slice(), 19).unwrap();
+  board.insert(0, 0xfb);
 
   board
 }
