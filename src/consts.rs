04c66cda0c832476d350122a23d5c5f13a9f4f37
yurzhang <yurzhang@163.com>
2023-02-02 14:38:13 +0800

    feat: get board outside the specified time

--- src/consts.rs
+++ src/consts.rs
@@ -1,3 +1,11 @@
+use chrono::NaiveTime;
+use lazy_static::lazy_static;
+
 // TODO(config)
 pub const WIDTH: u16 = 1000;
 pub const HEIGHT: u16 = 600;
+
+lazy_static! {
+  pub static ref BEGIN_TIME: NaiveTime = NaiveTime::from_hms_opt(15, 0, 0).unwrap();
+  pub static ref END_TIME: NaiveTime = NaiveTime::from_hms_opt(20, 0, 0).unwrap();
+}
